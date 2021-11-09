ad46c55075278b1aed0f5b8aae536c041b7cde9b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-11-09 16:19:13 -0500

    add sitemap generator

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // https://github.com/ekoeryanto/vuepress-plugin-sitemap
+    'sitemap': {
+      hostname: 'https://vuepress-cypress-test-example.netlify.app'
+    },
+  }
+}
