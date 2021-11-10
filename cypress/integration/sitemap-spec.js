e16a3e7de0f630e965beb3ad061397e3075c3b1d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-11-09 19:28:47 -0500

    add comments

--- cypress/integration/sitemap-spec.js
+++ cypress/integration/sitemap-spec.js
@@ -8,10 +8,12 @@ describe('sitemap', () => {
     cy.request('/sitemap.xml').its('body').then(body => {
       const x2js = new X2JS()
       const json = x2js.xml2js(body)
+      // get all URLs from the sitemap
       expect(json.urlset.url.length).to.be.greaterThan(0)
       json.urlset.url.forEach(url => {
         const parsed = new URL(url.loc)
         cy.log(parsed.pathname)
+        // check the response status and visit the page
         cy.request(url.loc).its('status').should('eq', 200)
         cy.visit(url.loc).wait(1000, {log: false})
       })
